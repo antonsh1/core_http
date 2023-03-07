9e3bed238893feb7a9be6d063e681e08ac71ba1d
Shelepov Anton <antonsh1@mail.ru>
2023-03-07 23:32:12 +0500

    Второе задание, отправка на проверку

--- src/main/java/ru/smartjava/Main.java
+++ src/main/java/ru/smartjava/Main.java
@@ -1,6 +1,5 @@
 package ru.smartjava;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.http.HttpHeaders;
 import org.apache.http.client.config.RequestConfig;
@@ -9,23 +8,25 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.entity.ContentType;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-import ru.smartjava.classes.Cat;
 import ru.smartjava.classes.NasaResponse;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 public class Main {
     public static void main(String[] args) {
 
+        ObjectMapper mapper = new ObjectMapper();
+        String path = "src/main/resources/";
         String nasaUrl = "https://api.nasa.gov/planetary/apod?api_key=";
         String nasaApiKey = "dnEd6FiO1wZxed9JzzBOq9R7hMjjFQnGz7JZJz9X";
         String URI = nasaUrl + nasaApiKey;
-        ObjectMapper mapper = new ObjectMapper();
 
+        NasaResponse nasaResponse;
         CloseableHttpClient httpClient = HttpClientBuilder.create()
                 .setDefaultRequestConfig(RequestConfig.custom()
                         .setConnectTimeout(5000)    // максимальное время ожидание подключения к серверу
@@ -34,21 +35,23 @@ public class Main {
                 .build();
 
         HttpGet request = new HttpGet(URI);
-
         request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
 
-        try {
-            CloseableHttpResponse response = httpClient.execute(request);
-            System.out.println(response.toString());
-            System.out.println(Arrays.toString(response.getEntity().getContent().readAllBytes()));
-            NasaResponse nasaResponse = mapper.readValue(response.getEntity().getContent(), new TypeReference<>() {});
-            System.out.println(nasaResponse);
-
+        //Обращаемся к ресурсу и преобразуем результат
+        try(CloseableHttpResponse response = httpClient.execute(request)) {
+            nasaResponse = mapper.readValue(response.getEntity().getContent(), NasaResponse.class);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-//        cats.stream().filter(cat -> cat.getUpvotes() != null && cat.getUpvotes() > 0).forEach(System.out::println);
+        //Берем последнюю часть url с названием файла
+        String fileName = nasaResponse.getUrl().split("/")[nasaResponse.getUrl().split("/").length - 1];
 
+        //Сохраняем файл с картинкой
+        try (InputStream in = new URL(nasaResponse.getUrl()).openStream()) {
+            Files.copy(in, Paths.get(path + fileName), StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/ru/smartjava/classes/NasaResponse.java
+++ src/main/java/ru/smartjava/classes/NasaResponse.java
@@ -1,15 +1,28 @@
 package ru.smartjava.classes;
 
-public class NasaResponse {
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-    String copyright;
-    String date;
-    String explanation;
-    String hdurl;
-    String media_type;
-    String service_version;
-    String title;
-    String url;
+public class NasaResponse {
+    @JsonProperty("copyright")
+    private String copyright;
+    @JsonProperty("date")
+    private String date;
+    @JsonProperty("explanation")
+    private String explanation;
+    @JsonProperty("hdurl")
+    private String hdurl;
+    @JsonProperty("media_type")
+    private String media_type;
+    @JsonProperty("service_version")
+    private String service_version;
+    @JsonProperty("title")
+    private String title;
+    @JsonProperty("url")
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
 
     @Override
     public String toString() {
